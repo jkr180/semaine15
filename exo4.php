27b80404cc50cbdbd60f7acb5ad381e1cf63d4bb
Lucas <lucaslocatin180@gmail.com>
2021-04-16 10:18:43 +0200

    message

---
+++ exo4.php
@@ -0,0 +1,33 @@
+<?php
+$a = array("19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
+       "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
+       "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
+     );
+     var_dump($a[0]);
+     var_dump($a);
+     $tab[] = "Banane"; 
+     var_dump($tab);
+?>
+
+Exercice 0
+Testez les différents exemples du cours, à l'aide de la fonction var_dump().
+
+Exercice 1
+Quelle semaine a lieu la validation du groupe 19002 ?
+
+a la semaine 24
+
+Exercice 2
+Trouver la position de la dernière occurrence de Stage pour le groupe 19001.
+
+ a la semaine 23
+
+Exercice 3
+Extraire, dans un nouveau tableau, les codes des groupes.
+
+
+Exercice 4
+Combien de semaines dure le stage du groupe 19003 ?
+12 semaine
+
+Présentez votre travail à un formateur.
